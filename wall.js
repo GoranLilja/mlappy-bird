33e51670c667dabbcd617e7b1416130cd9e27b2b
Göran Lilja <goran@familjenlilja.se>
2023-11-24 14:41:07 +0100

    Update wall.js

--- wall.js
+++ wall.js
@@ -2,7 +2,7 @@ class Wall {
     constructor(w = 20) {
         this.x = width
         this.h = random(20, 300)
-        this.gap = random(20, 30)
+        this.gap = random(20, 50)
         this.w = w
     }
 
